cedd81a2daef9dcc51b069a1b3f544c5613a5619
UrsaMinoR3 <davidpedraza41@gmail.com>
2022-12-01 11:48:49 -0500

    new version

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
-fetch('https://swapi.dev/api/people/1')
-.then(response => response.json())
-.then(data => 
-    console.log(data)
-)
+const name = document.getElementById("name");
+const eyecolor = document.getElementById("eyecolor");
+const button = document.querySelector(".getRandomCharacter");
 
+button.addEventListener('click',(e) => {
+    e.preventDefault()
+    const randomNumber =  Math.ceil(Math.random() * 83)
+    fetch(`https://swapi.dev/api/people/${randomNumber}/`)
+        .then(response => response.json())
+        .then(character => {
+            name.innerHTML = character['name'];
+            eyecolor.innerHTML = character['eye_color'];
+        })
+})       
